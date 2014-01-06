a1493ae6af7c6b56d8f82ee0ff004bf9d93e303d
tsakov <etsakov@asteasolutions.com>
2014-01-06 23:41:37 +0100

    The balance for each bank account is now visible on the home page.

--- src/main/java/lu/uni/moneymanager/jpa/facade/PaymentFacade.java
+++ src/main/java/lu/uni/moneymanager/jpa/facade/PaymentFacade.java
@@ -14,4 +14,20 @@ public class PaymentFacade extends AbstractFacade {
         return em.createQuery("from Payment p where p.bankAccount = :bankAccount", Payment.class)
                 .setParameter("bankAccount", bankAccount).getResultList();
     }
+
+    public double getBalanceForBankAccount(BankAccount bankAccount) {
+        Double incoming = em.createQuery("SELECT SUM(p.amount) FROM Payment p WHERE p.bankAccount = :bankAccount AND p.status = 'incoming'", Double.class)
+                .setParameter("bankAccount", bankAccount).getSingleResult();
+        if (incoming == null) {
+            incoming = new Double(0);
+        }
+
+        Double outgoing = em.createQuery("SELECT SUM(p.amount) FROM Payment p WHERE p.bankAccount = :bankAccount AND p.status = 'outgoing'", Double.class)
+                .setParameter("bankAccount", bankAccount).getSingleResult();
+        if (outgoing == null) {
+            outgoing = new Double(0);
+        }
+
+        return incoming.doubleValue() - outgoing.doubleValue();
+    }
 }
